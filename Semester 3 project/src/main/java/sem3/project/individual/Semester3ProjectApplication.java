c0c4d421a6162764f4de92d96aa3bc42eec2f229
Yannick <y.gorissen@student.fontys.nl>
2023-03-03 14:13:04 +0100

    project generation

---
+++ Semester 3 project/src/main/java/sem3/project/individual/Semester3ProjectApplication.java
@@ -0,0 +1,15 @@
+package sem3.project.individual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semester3ProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Semester3ProjectApplication.class, args);
+    }
+
+}
---
+++ Semester 3 project/src/test/java/sem3/project/individual/Semester3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sem3.project.individual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semester3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

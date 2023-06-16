41c4647d509ea9deb0af860b5dc9050e4016466b
Yannick <y.gorissen@student.fontys.nl>
2023-06-16 17:33:00 +0200

    websockets support

---
+++ Semester 3 project/src/main/java/sem3/project/individual/domain/notifications/NotificationMessage.java
@@ -0,0 +1,12 @@
+package sem3.project.individual.domain.notifications;
+
+import lombok.Data;
+
+@Data
+public class NotificationMessage {
+    private String id;
+    private String from;
+    private String to;
+    private String text;
+}
+
